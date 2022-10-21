9909e43f4e07199d6782b0b29c5a35e9ab1054b0
Natasha Eater <natalya.iter@yandex.ru>
2022-10-21 15:31:24 +0300

    dzone

---
+++ zadacha10/zadachaone.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Ввведите трехзначное число ");
+int numberA = int.Parse(Console.ReadLine());
+int numberB=(numberA/10)%10;
+
+if (numberA>-999 && numberA<999)
+{
+    Console.WriteLine($"{numberB},");
+}
+else 
+{
+Console.WriteLine("Данное число не соответствует условию");
+}
