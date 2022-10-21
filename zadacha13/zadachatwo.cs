2864804d24129d38f2d3f63abde0d0cfc6bc3d8e
Natasha Eater <natalya.iter@yandex.ru>
2022-10-21 17:06:36 +0300

    dz2

--- zadacha13/zadachatwo.cs
+++ zadacha13/zadachatwo.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Ввведите число ");
+int number = int.Parse(Console.ReadLine()??"0");
+
+while(number>1000||number<-1000)
+{
+    number=number/10;
+}
+if (number>99||number<-99)
+{
+    int count=(number%10);
+    Console.WriteLine(count);
+}
+else 
+{
+   Console.WriteLine("Третьею цифру невозможно определить "); 
+}
