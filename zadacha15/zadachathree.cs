6a82c92b1e47074a2fbd459f65db58e68d8a9109
Natasha Eater <natalya.iter@yandex.ru>
2022-10-21 17:25:52 +0300

    dz3

--- zadacha15/zadachathree.cs
+++ zadacha15/zadachathree.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//Данный код был сдан к предыдущему семинару.
+Console.Clear();
+Console.WriteLine("Введите номер недели от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+if (number>0 && number<8)
+{
+    if (number<6 && number>0)
+{
+    Console.WriteLine("Будний день");
+}
+    if (number==6 || number==7)
+{
+    Console.WriteLine("Сегодня выходной");
+}
+}
+else
+{
+Console.WriteLine("Число не соответствует условию ");
+}
